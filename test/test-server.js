62b677cbfd295f74a81db078ccd6f98f25ba5869
Cameron Dodd <cameron.dodd@mavs.uta.edu>
2017-12-11 21:12:31 -0500

    added done() to test

--- test/test-server.js
+++ test/test-server.js
@@ -8,12 +8,13 @@
 
  describe('API', function() {
 
-   it('should 200 on GET requests', function() {
+   it('should 200 on GET requests', function(done) {
      return chai.request(app)
        .get('/api/fooooo')
        .then(function(res) {
          res.should.have.status(200);
          res.should.be.json;
+         done();
        });
    });
  });
